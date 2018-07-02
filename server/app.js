7f09b4f84b4048dc39c8a552c9022821d17fbd2f
narut <narut@NARUTs-MacBook-Air.local>
2018-07-02 13:13:05 +0700

    Install cors

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
+
+// allow cross-origin requests
+app.use(cors)
 
 mongoose.connect('mongodb://net:test1234@ds223161.mlab.com:23161/gql-practice')
 mongoose.connection.once('open', () => {
