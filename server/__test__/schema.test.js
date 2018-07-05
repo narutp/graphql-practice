66bfd6eb99e0bac203cf90f0336b137613d87163
narut <narut@NARUTs-MacBook-Air.local>
2018-07-05 16:07:49 +0700

    Config test envi of jest to run in node envi

--- server/__test__/schema.test.js
+++ server/__test__/schema.test.js
@@ -1,1 +1,27 @@
-import axios from 'axios'
+const axios = require('axios')
+
+describe('schema', () => {
+    it('find user id and title', async() => {
+        const res = await axios.post('http://localhost:4000/graphql', {
+            query: 
+            `
+            {
+                user(id: 1) {
+                  id
+                  title
+                }
+            }
+            `
+        })
+        expect(res.data).toMatchObject(
+            {
+                "data": {
+                  "user": {
+                    "id": "1",
+                    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+                  }
+                }
+            }
+        )
+    })
+})
